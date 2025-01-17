a084ef23857c5b224be7532d690d605a79d12f3c
HMJ1992 <hutianshi@live.cn>
2025-01-17 13:52:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '认清生活的真相后还依然热爱生活。',
+    '认清生活的真相后还依然热爱生活',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
